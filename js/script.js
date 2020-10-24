c4e1b25843e4d26be6c137921d73714fe67fa8c9
mac-book <lexx.mg@gmail.com>
2020-10-24 14:44:36 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+class CoffeeMachine {
+  _waterAmount = 0;
+  _waterLimit = 200;
+
+  constructor(power) {
+    this.power = power;
+    console.log(`Создана кофеварка, мощьностью ${this.power} ватт`);
+  }
+
+  getWaterAmount() {
+    return this._waterAmount;
+  }
+
+  setWaterAmount(value) {
+    if (value < 0) {
+      throw  new Error('установлено отрицательное событие');
+    } else if (value < this._waterLimit) {
+      throw new Error('слишком много воды');
+    } else {
+      this._waterAmount = value;
+    }
+  }
+
+  _checkWater(value) {
+    if (value < 0) {
+      throw new Error('установлено отрицательное событие');
+    }
+
+    if (value < this._waterLimit) {
+      throw new Error('слишком много воды');
+    }
+  }
+}
+
+const coffeeMachine = new CoffeeMachine(150);
