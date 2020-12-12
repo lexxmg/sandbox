ebd81ec76aa460100adcc9b843c7ce383501fa34
mac-mini <lexx.mg@gmail.com>
2020-12-12 11:05:42 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const btn = document.querySelector('.btn');
+const btn1 = document.querySelector('.btn1');
+const btn2 = document.querySelector('.btn2');
+
+const foo = (callback) => {
+  callback();
+}
+
+btn.addEventListener('click', () => {
+  callback();
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+  console.log('this инутри вызова' + ' ' + this);
+  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
+
+btn1.addEventListener('click', function() {
+  callback();
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+  console.log('this инутри вызова' + ' ' + this);
+  console.log('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+});
+
+btn2.addEventListener('click', callback);
+
+function callback() {
+  console.log('____________________________');
+  console.log('функция callback вызвана');
+  console.log('значение this');
+  console.log(this);
+  console.log('____________________________');
+}
