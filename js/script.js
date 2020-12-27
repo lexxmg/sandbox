75600b243e74a5b9b61f0217ec751c39b83dad40
mac-mini <lexx.mg@gmail.com>
2020-12-27 22:34:58 +0300

    sync

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const a = {
+  primitive: 'string',
+  boolean: true,
+  boolean: 'true___',
+  array: [1, 4, 'str'],
+  obj: {
+    objInner: {
+      name: 'vasya',
+      age: 18
+    }
+  }
+}
+
+const b = {...a, boolean: 0};
+// b.array = [...a.array];
+// b.obj = {...a.obj};
+// b.obj.objInner = {...a.obj.objInner};
