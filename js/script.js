5dd4b156e24e83596d243c8970db6b32dc450754
mac-mini <lexx.mg@gmail.com>
2020-12-30 07:16:21 +0300

    sync

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const a = {
   boolean: true,
   boolean: 'true___',
   array: [1, 4, 'str'],
+  arrObj: [{name: 'vasya1',age: 18}, {name: 'vasya2',age: 28}, {name: 'vasya3',age: 58}],
   obj: {
     objInner: {
       name: 'vasya',
@@ -17,3 +18,10 @@ const b = {...a, boolean: 0};
 // b.array = [...a.array];
 // b.obj = {...a.obj};
 // b.obj.objInner = {...a.obj.objInner};
+const c = a.arrObj.map(obj => {
+  if (obj.age === 28) {
+    return {...obj, age: 69};
+  }
+
+  return obj;
+});
