6e6816d41ab038285c4cded37fc7e9651f3df93d
mac-mini <lexx.mg@gmail.com>
2021-01-10 20:26:41 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+
 'use strict';
+
+const arr = [2, 4, 5, 6, 9];
+
+const foo = (num) => {
+  return (el) => {
+    return el !== num;
+  }
+}
+
+console.log(arr);
+console.log( arr.filter(foo(4)) );
+//console.log( arr.some(foo(44)) );
+//console.log( arr.find(foo(4)) );
