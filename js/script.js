5fbe4e9ec156a20fd5e421457a25be43c6af0cf5
mac-mini <lexx.mg@gmail.com>
2021-04-20 10:34:10 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
+
 'use strict';
+
+const users = [
+  {id: 1, name: 'вася', age: 16},
+  {id: 2, name: 'петя', age: 22},
+  {id: 3, name: 'саша', age: 17},
+  {id: 4, name: 'даша', age: 19}
+];
+
+console.log(users);
+
+console.log( reInndexArr(users, 'id') );
+
+console.log( reInndexArr(users, 'id')[4] );
+
+console.log( reInndexArr(users, 'name').саша );
+
+
+function reInndexArr(arr, key) {
+  let result = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    result[arr[i][key]] = arr[i];
+  }
+
+  return result;
+}
